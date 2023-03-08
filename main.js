6d36611fd302d193683919b33161c190d5f7dec6
MSMANI12 <selvamani12121998@gmail.com>
2023-03-08 10:13:09 +0530

    add assending-ifelse

---
+++ main.js
@@ -0,0 +1,35 @@
+var x=2,y=4,z=6;
+
+if((x<y)&&(x<y)){
+
+		console.log(x+" "+y+" "+z);
+	}
+	
+	
+var a=8,b=4,c=6;
+if((a<b)&&(a<c)){
+	if(b<c){
+		console.log(a,b,c);
+	}
+	else{
+		console.log(a,c,b);
+	}
+}
+	if((b<a)&&(b<c)){
+		if(a<c)
+		{
+			console.log(b,a,c);
+		}
+		else{
+			console.log(b,c,a);
+		}
+	}
+	if((c<a)&&(c<b)){
+		if(a<b){
+			console.log(c,b,a);
+		}
+		else{
+			console.log(c,a,b);
+		}
+	}
+		
